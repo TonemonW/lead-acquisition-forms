9d0bbd545376a0a7826dae9b82cdeab7a9377cf8
Tong <tong0365@gmail.com>
2025-08-10 21:11:11 +1000

    Update:public

--- backend/types/loanForm.d.ts
+++ backend/types/loanForm.d.ts
@@ -1,23 +1,23 @@
-import type { z } from 'zod';
+import { z } from 'zod';
+export declare const loanTypeEnum: z.ZodEnum<["Personal Loan", "Home Loan", "Car Loan"]>;
+export type LoanType = z.infer<typeof loanTypeEnum>;
 export declare const loanFormSchema: z.ZodObject<{
     loanAmount: z.ZodEffects<z.ZodString, string, string>;
-    loanType: z.ZodEffects<z.ZodTypeAny, any, any>;
+    loanType: z.ZodEffects<z.ZodEnum<["Personal Loan", "Home Loan", "Car Loan"]>, "Personal Loan" | "Home Loan" | "Car Loan", unknown>;
     fullName: z.ZodString;
     email: z.ZodString;
     phoneNumber: z.ZodString;
 }, "strip", z.ZodTypeAny, {
     loanAmount: string;
-    loanType?: any;
+    loanType: "Personal Loan" | "Home Loan" | "Car Loan";
     fullName: string;
     email: string;
     phoneNumber: string;
 }, {
     loanAmount: string;
-    loanType?: any;
     fullName: string;
     email: string;
     phoneNumber: string;
+    loanType?: unknown;
 }>;
 export type LoanFormData = z.infer<typeof loanFormSchema>;
-
-
